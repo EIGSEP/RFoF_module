154d568db7e704d22b3c3fced8ae82e33b464f17
planetaryCat <richardjs@berkeley.edu>
2025-05-07 10:37:11 -0700

    added functionality so every time we run the script the attenuation does not have to be set...

--- scripts/transmitter.py
+++ scripts/transmitter.py
@@ -22,19 +22,18 @@ def main():
     parser.add_argument(
         "--atten",
         type=float,
-        required=True,
-        help="Attenuation to set in dB."
+        help="Set attenuation in dB (0 - 31.5)"
     )
 
     parser.add_argument(
         "--read_temp",
-        type=float,
+        action="store_true",
         help="Reads temperature of Ftx board."
     )
 
     parser.add_argument(
         "--read_atten",
-        type=float,
+        action="store_true",
         help="Reads attenuation of Ftx board."
 
     )
@@ -52,27 +51,31 @@ def main():
         sys.exit(1)
 
     # Set attenuation MAX ATTENUATION 31.5 dB
-    try:
-        tx.set_atten(args.atten)
-        print(f"Successfully set attenuation to {args.atten} dB")
-    except Exception as e:
-        print(f"Error setting attenuation: {e}")
-        sys.exit(1)
+    if args.atten is not None:
+        try:
+            tx.set_atten(args.atten)
+            print(f"Successfully set attenuation to {args.atten} dB")
+        except Exception as e:
+            print(f"Error setting attenuation: {e}")
+            sys.exit(1)
 
     # Grab temp in C˚
-    try:
-        tx.get_temp(args.read_temp)
-        print(f"Transmitter Temp: {args.read_temp} C˚")
-    except Exception as e:
-        print(f"Failed to read temperature: {e}")
-        sys.exit(1)
+    if args.read_temp:
+        try:
+            temp = tx.get_temp()
+            print(f"Transmitter Temp: {temp:.2f} C˚")
+        except Exception as e:
+            print(f"Failed to read temperature: {e}")
+            sys.exit(1)
 
     # Readout attenuation of Ftx board (dB).
-    try:
-        tx.get_atten(args.read_atten)
-    except Exception as e:
-        print(f"Failed to read Ftx attenuation. {e}")
-        sys.exit(1)
+    if args.read_atten:
+        try:
+            atten = tx.get_atten()
+            print(f"attenuation: {atten} dB.")
+        except Exception as e:
+            print(f"Failed to read Ftx attenuation. {e}")
+            sys.exit(1)
     
 
 if __name__ == "__main__":
